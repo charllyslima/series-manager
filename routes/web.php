a18d2633b7637b0726c1359559082a9f0c3f0bf1
charllys <charllys.lima@grancursosonline.com.br>
2022-12-29 17:51:51 -0300

    add bootstrap in vite

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return view('welcome');
 });
+    Route::get('/home', static function () {
+        return view('home');
+    });
